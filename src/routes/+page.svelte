06f1a5b3638c02a3f675aeb033e409b1811f3c86
Soumil <soumil009@outlook.com>
2023-07-08 10:56:44 +0100

    created homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-5xl">Guess Items by Their Picture</h1>
+
+<div>
+    <a href="/join-game" class="btn">Join a Game</a>
+    <a href="/create-game" class="btn">Create a Game</a>
+</div>
